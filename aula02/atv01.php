468539d6063d3596e264bd21a1c916c34ef5fd53
Vinicius <145489097+vinicius-moraisdesouza@users.noreply.github.com>
2023-10-18 19:12:21 -0300

    Add files via upload

---
+++ aula02/atv01.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="atv01.css">
+</head>
+<body>
+
+
+    <div class="div1">
+       <div class="div4">
+           <h1>Black</h1>
+       </div>
+    </div>
+
+
+    <div class="div2">
+
+    </div>
+
+
+    <div class="div3">
+
+    </div>
+
+
+</body>
+</html>
---
+++ aula02/exp02.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="exp02.css">
+</head>
+<body>
+
+    <h1>titulo</h1>
+
+    <p class= "stile">  classe do paragrafo</p>
+
+    <p> p de fora </p>
+
+    <div class= "conteudo">
+        <p> titulo da div</p>
+
+        <p id= "txt"> texto fora do subconteudo</p>
+
+    </div>
+
+
+
+</body>
+</html>
